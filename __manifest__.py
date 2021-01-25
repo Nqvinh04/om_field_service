5b30c4c8a218f96dbb43cb85657241bb4d02b8a7
nqvinh04 <nqvinh1995@gmail.com>
2021-01-25 16:24:12 +0700

    update

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'Sale Manager',
+    'version': '13.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Odoo Development Tutorials For Beginners',
+    'sequence': '10',
+    'license': 'AGPL-3',
+    'author': 'NQ Vinh',
+    'maintainer': 'Odoo Mates',
+    'website': 'odoomates.com',
+    'live_test_url': 'https://www.youtube.com/watch?v=BDepk0LhVuI&list=PLqRRLx0cl0hoJhjFWkFYowveq2Zn55dhM&index=1',
+    'depends': ['sale', 'mail', 'board', 'website'],
+    'demo': [],
+    'data': [
+    ],
+    'images': ['static/description/banner.png'],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
