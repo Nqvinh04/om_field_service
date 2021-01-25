c7fe30785bc4e8102cbece9d7630146dd3dc30e5
nqvinh04 <nqvinh1995@gmail.com>
2021-01-25 17:57:18 +0700

    update file 25/01

--- __manifest__.py
+++ __manifest__.py
@@ -12,8 +12,9 @@
     'depends': ['sale', 'mail', 'board', 'website'],
     'demo': [],
     'data': [
+        # 'views/sale_view.xml'
     ],
-    'images': ['static/description/banner.png'],
+    # 'images': ['static/description/banner.png'],
     'installable': True,
     'application': True,
     'auto_install': False,
---
+++ models/sale.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api, _
+
+
+class SaleOrderLine(models.Model):
+    _inherit = 'sale.order.line'
+
+    # assign =
+
